3cd3505bd158bacb98d9fbce0335ea917f4debdf
Niko <nikolaos.hubert@gmail.com>
2020-01-03 18:06:35 +0100

    Niko modif AccueilPage avec bouton creer projet

--- src/main/java/groupe2/libreplan/AccueilPage.java
+++ src/main/java/groupe2/libreplan/AccueilPage.java
@@ -1,10 +1,15 @@
 package groupe2.libreplan;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class AccueilPage extends MenuHaut {
 
+	@FindBy (xpath = "//img[@src='/libreplan/common/img/ico_add.png']")
+	WebElement buttonCreerProjet;
+	
 	
 	public AccueilPage(WebDriver driver) {
         super();
