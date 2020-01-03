06628fa93e97a5f493505fc4c808f7333d6b8fe6
RevanSurGitHub <revan.en.vadrouille+github@gmail.com>
2020-01-03 18:13:14 +0100

    AMA TypesHeuresPage + CategorieCoutPage jusqu a fin pas 5

--- src/main/java/groupe2/libreplan/CategoriesCoutPage.java
+++ src/main/java/groupe2/libreplan/CategoriesCoutPage.java
@@ -5,26 +5,125 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class CategoriesCoutPage {
+public class CategoriesCoutPage extends MenuHaut {
 
 	// Pour constructeur
 	WebDriver driver ;
-	
-	// Fil d'Ariane 2 : Coût
-	@FindBy (xpath = "//tr[@class=\"ruta\"]//td[4]/table[2]/tbody/tr/td/table/tbody//tr/td[1]/table/tbody/tr/td/table/tbody/tr/td[3]/span")
-	WebElement contenuCout ;
-	
-	// Fil d'Ariane 3 : Catégories de coût
-	@FindBy (xpath = "//tr[@class=\"ruta\"]//td[4]/table[2]/tbody/tr/td/table/tbody//tr/td[3]/table/tbody/tr/td/table/tbody/tr/td[3]/span")
-	WebElement contenuCategorieDeCout ;
 	
 	// Bouton Créer
 	@FindBy (xpath = "//td[contains(text(), \"Créer\")]")
 	WebElement boutonCreer ;
 	
+	// Titre colonne 1 : Nom de catégorie
+	@FindBy (xpath = "//div[@class=\"clickable-rows z-grid\"]//th[1]//div[@class=\"z-column-sort-img\"]")
+	WebElement titreColonneNomCategorie ;
+	
+	// Titre colonne 1 : Activé
+	@FindBy (xpath = "//div[@class=\"clickable-rows z-grid\"]//th[2]//div[@class=\"z-column-sort-img\"]")
+	WebElement titreColonneActive ;
+	
+	// Titre colonne 1 : Actions
+	@FindBy (xpath = "//div[@class=\"clickable-rows z-grid\"]//th[3]//div[@class=\"z-column-sort-img\"]")
+	WebElement titreColonneActions ;
+	
+	
 	// ________________________________________Formulaire Créer une Catégorie de Coûts__________________________________________________________
 	
+	// Titre page Créer Catégorie de coût
+	@FindBy (xpath = "//td[@class=\"z-caption-l\"]")
+	WebElement contenuTitreCreerCategorie ;
 	
+	// Titre onglet : Donnée de catégorie
+	@FindBy (xpath = "//span[@class=\"z-tab-text\"]")
+	WebElement titreOnglet ;
+	
+	// Champ Code Disabled (si pb xpath : //input[@class=\"z-textbox z-textbox-disd z-textbox-text-disd\"] )
+	@FindBy(xpath = "//td[1]/input")
+	WebElement champCodeDisabled;
+	
+	// Champ du Code (si pb xpath : //tr[@valign=\"top\"]//input[@class=\"z-textbox\"] )
+	@FindBy(xpath = "//td[1]/input")
+	WebElement champCode;
+
+	// Checkbox "Générer le code" (si pb xpath : //tr[@valign=\"top\"]//input[@type=\"checkbox\"] )
+	@FindBy(xpath = "//td[3]/span/input")
+	WebElement cbGenererLeCode;
+	
+	// Champ du Nom (si pb xpath : //input[@class=\"z-textbox\"] )
+	@FindBy(xpath = "//div/input")
+	WebElement champNom;
+	
+	// Checkbox : Activé
+	@FindBy (xpath ="//div/span/input[@type=\"checkbox\"]")
+	WebElement cbActive ;
+	
+	// Titre block : Coûts des heures
+	@FindBy (xpath = "//tr[3]/td/div/div[1]")
+	WebElement titreBlockCouts ;
+	
+	// Bouton : Ajouter une ligne (si pb xpath : //tr[3]//td[@class="z-button-cm"] )
+	@FindBy (xpath = "//div/table/tbody/tr/td/table/tbody/tr/td/span/table/tbody/tr[2]/td[2]")
+	WebElement boutonAjouterUneLigne ;
+		
+	// Tableau : Coûts des heures > colonne : Code
+	@FindBy (xpath = "//div[@class=\"z-panel-body\"]//th[1]/div[@class=\"z-column-cnt\"]")
+	WebElement titreColonneCode ;
+	
+	// Tableau : Coûts des heures > colonne : Type
+	@FindBy (xpath = "//div[@class=\"z-panel-body\"]//th[2]/div[@class=\"z-column-cnt\"]")
+	WebElement titreColonneType ;
+	
+	// Tableau : Coûts des heures > colonne : Coût horaire
+	@FindBy (xpath = "//div[@class=\"z-panel-body\"]//th[3]/div[@class=\"z-column-cnt\"]")
+	WebElement titreColonneCoutHoraire ;
+	
+	// Tableau : Coûts des heures > colonne : Date de début
+	@FindBy (xpath = "//div[@class=\"z-panel-body\"]//th[4]/div[@class=\"z-column-cnt\"]")
+	WebElement titreColonneDateDebut ;
+	
+	// Tableau : Coûts des heures > colonne : Date de fin
+	@FindBy (xpath = "//div[@class=\"z-panel-body\"]//th[5]/div[@class=\"z-column-cnt\"]")
+	WebElement titreColonneDateFin ;
+	
+	// Tableau : Coûts des heures > colonne : Opérations
+	@FindBy (xpath = "//div[@class=\"z-panel-body\"]//th[5]/div[@class=\"z-column-cnt\"]")
+	WebElement titreColonneOperations ;
+	
+	// Bouton : Enregistrer
+	@FindBy (xpath = "//span[@class=\"save-button global-action z-button\"]//td[@class=\"z-button-cm\"]")
+	WebElement boutonEnregistrer ;
+	
+	// Bouton : Sauvegarder et continuer
+	@FindBy (xpath = "//span[@class=\"saveandcontinue-button global-action z-button\"]//td[@class=\"z-button-cm\"]")
+	WebElement boutonSauvegarder ;
+	
+	// Bouton : Annuler
+	@FindBy (xpath = "//span[@class=\"cancel-button global-action z-button\"]//td[@class=\"z-button-cm\"]")
+	WebElement boutonAnnuler ;
+	
+	// Champ : Code Disabled dans Tableau Coûts des heures
+	@FindBy (xpath = "//div/input[@class=\"z-textbox z-textbox-disd z-textbox-text-disd\"])")
+	WebElement champCodeTableau ;
+	
+	// Menu déroulant : Type dans Tableau Coûts des heures
+	@FindBy (xpath = "//select")
+	WebElement selectType;
+	
+	// Champ : Coût Horaire dans Tableau Coûts des heures
+	@FindBy (xpath = "//input[@class=\"z-decimalbox\"]")
+	WebElement champCoutHoraireTableau ;
+	
+	// Champ : Date de début dans Tableau Coûts des heures
+	@FindBy (xpath = "//tr[@class=\"z-row\"]/td[4]//input[@class=\"z-datebox-inp\"]")
+	WebElement champDateDeDebut ;
+	
+	// Champ : Date de fin dans Tableau Coûts des heures
+	@FindBy (xpath = "//tr[@class=\"z-row\"]/td[5]//input[@class=\"z-datebox-inp\"]")
+	WebElement champDateDeFin ;
+	
+	// Icone : poubelle pour supprimer la ligne dans Tableau Coûts des heures
+	@FindBy (xpath  ="//span[@title=\"Supprimer\"]//img")
+	WebElement iconeSupprimer ;
 	
 	public CategoriesCoutPage (WebDriver driver) {
         super();
