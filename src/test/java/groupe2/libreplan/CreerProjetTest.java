b047ce316702390eb7d2bb7a6e312ec96b2d64ed
Niko <nikolaos.hubert@gmail.com>
2020-01-06 17:26:05 +0100

    Modif CreerProjetTest

--- src/test/java/groupe2/libreplan/CreerProjetTest.java
+++ src/test/java/groupe2/libreplan/CreerProjetTest.java
@@ -39,6 +39,7 @@ public class CreerProjetTest {
 			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
 			login = new LoginPage(driver);
+			// _______________Step 1_______________  
 			// Remplir le champ login et mot de passe, puis cliquer sur le bouton valider
 			SocleTechnique.RemplirLoginMdp(login.champUtilisateur, login.champPassword, login.boutonSeConnecter, User, MDP);
 
@@ -49,7 +50,7 @@ public class CreerProjetTest {
 			accueil = new AccueilPage(driver);
 			//Cliquer sur le bouton creer projet
 			accueil.buttonCreerProjet.click();
-			
+			//_______________Step 2_______________ 
 			//Vérifier les champs proposé par la fenêtre de création de projet
 			//Champ Nom
 			SocleTechnique.assertEqualsLogger("Le titre du champ n'est pas Nom", "Nom", accueil.tilteNom.getText(), logger);
@@ -95,7 +96,8 @@ public class CreerProjetTest {
 			SocleTechnique.assertEqualsLogger("Le menu déroulant Calendrier n'est pas positionné sur Default", "Default", accueil.selectCalendrier.getAttribute("value"), logger);
 			logger.info("Dans la fenêtre de création de projet le menu déroulant Calendrier est bien positionné sur Default");
 			
-			//Remplir les champs
+			//_______________Step 3_______________ 
+			//Remplir les champs et création d'un projet
 			//Champ Nom
 			SocleTechnique.RemplirChamp(accueil.champNom, "PROJET_TEST1");
 			//Décocher Générer le code et remplir le champ Code
@@ -107,13 +109,83 @@ public class CreerProjetTest {
 			//Champ Echéance
 			datF = SocleTechnique.DateFuture(15);
 			SocleTechnique.RemplirChamp(accueil.champEcheance, datF);
+			//Cliquer sur Accepter
+			accueil.buttonAccepter.click();
+			
+			//Accès à la page Detail Projet
+			detailProjet = new DetailProjetPage(driver);
+			//Vérification des éléments de la page
+			SocleTechnique.assertTrueLogger("Le menu vertical Détail du projet n'est pas séléctionné", detailProjet.menuDetailProjet.isSelected(), logger);
+			logger.info("Le menu vertical Détail du projet est séléctionné");
+			SocleTechnique.assertTrueLogger("L'onglet WBS (tâches) n'est pas séléctionné", detailProjet.ongletWBS.isSelected(), logger);
+			logger.info("L'onglet WBS (tâches) est séléctionné");
+			
+			//_______________Step 4_______________ 
+			//Vérification des éléments du menu vertical
+			SocleTechnique.assertEqualsLogger("Le nom du premier menu vertical n'est pas Planification de projet", "Planification de projet", detailProjet.menuPlanificationProjet.getText(), logger);
+			logger.info("Le nom du premier menu vertical est bien Planification de projet");
+			SocleTechnique.assertEqualsLogger("Le nom du deuxième menu vertical n'est pas Détail du projet", "Détail du projet", detailProjet.menuDetailProjet.getText(), logger);
+			logger.info("Le nom du deuxième menu vertical est bien Détail du projet");
+			SocleTechnique.assertEqualsLogger("Le nom du troisième menu vertical n'est pas Chargement des ressources", "Chargement des ressources", detailProjet.menuChargementRessources.getText(), logger);
+			logger.info("Le nom du troisième menu vertical est bien Chargement des ressources");
+			SocleTechnique.assertEqualsLogger("Le nom du quatrième menu vertical n'est pas Allocation avancée", "Allocation avancée", detailProjet.menuAllocationAvance.getText(), logger);
+			logger.info("Le nom du quatième menu vertical est bien Allocation avancée");
+			SocleTechnique.assertEqualsLogger("Le nom du cinquième menu vertical n'est pas Tableau de bord", "Tableau de bord", detailProjet.menuTableauBord.getText(), logger);
+			logger.info("Le nom du cinquième menu vertical est bien Tableau de bord");
+			
+			//_______________Step 5_______________ 
+			//Vérification des onglets
+			SocleTechnique.assertTrueLogger("Les onglets ne sont pas affichés", detailProjet.listOnglets.isDisplayed(), logger);
+			logger.info("Les onglets sont affichés");
+			SocleTechnique.assertEqualsLogger("Le nom du premier onglet n'est pas WBS (tâches)", "WBS (tâches)", detailProjet.ongletWBS.getText(), logger);
+			logger.info("Le nom du premier onglet est bien WBS (tâches)");
+			SocleTechnique.assertEqualsLogger("Le nom du deuxième onglet n'est pas Données générales", "Données générales", detailProjet.ongletDonneesGenerales.getText(), logger);
+			logger.info("Le nom du premier onglet est bien Données générales");
+			SocleTechnique.assertEqualsLogger("Le nom du troisième onglet n'est pas Coût", "Coût", detailProjet.ongletCout.getText(), logger);
+			logger.info("Le nom du premier onglet est bien Coût");
+			SocleTechnique.assertEqualsLogger("Le nom du quatième onglet n'est pas Avancement", "Avancement", detailProjet.ongletAvancement.getText(), logger);
+			logger.info("Le nom du premier onglet est bien Avancement");
+			SocleTechnique.assertEqualsLogger("Le nom du cinquième onglet n'est pas Libellés", "Libellés", detailProjet.ongletLibelles.getText(), logger);
+			logger.info("Le nom du premier onglet est bien Libellés");
+			SocleTechnique.assertEqualsLogger("Le nom du sixième onglet n'est pas Exigence de critère", "Exigence de critère", detailProjet.ongletExigenceCritere.getText(), logger);
+			logger.info("Le nom du premier onglet est bien Exigence de critère");
+			SocleTechnique.assertEqualsLogger("Le nom du septième onglet n'est pas Matériels", "Matériels", detailProjet.ongletMateriels.getText(), logger);
+			logger.info("Le nom du premier onglet est bien Matériels");
+			SocleTechnique.assertEqualsLogger("Le nom du huitième onglet n'est pas Formulaires qualité des tâches", "Formulaires qualité des tâches", detailProjet.ongletFormulaireQualite.getText(), logger);
+			logger.info("Le nom du premier onglet est bien Formulaires qualité des tâches");
+			SocleTechnique.assertEqualsLogger("Le nom du neuvième onglet n'est pas Autorisation", "Autorisation", detailProjet.ongletAutorisation.getText(), logger);
+			logger.info("Le nom du premier onglet est bien Autorisation");
+			
+			//_______________Step 6_______________ 
+			//Verifier la présence des boutons enregistrer le projet et annuler l'édition et de leurs icones
+			SocleTechnique.assertTrueLogger("Le bouton Annuler l'édition n'est pas affiché", detailProjet.buttonAnnulerEdition.isDisplayed(), logger);
+			logger.info("Le bouton Annuler l'édition est bien affiché");
+			SocleTechnique.assertEqualsLogger("Le bouton Annuler l'édition n'est pas représenté par le bonne icone", "http://localhost:8090/libreplan/common/img/ico_back.png", detailProjet.buttonAnnulerEdition.getAttribute("src"), logger);
+			logger.info("Le bouton Annuler l'édition est bien représenté par le bon icone");
+			SocleTechnique.assertTrueLogger("Le bouton Enregistrer Projet n'est pas affiché", detailProjet.buttonEnregistrerProjet.isDisplayed(), logger);
+			logger.info("Le bouton Enregistrer le projet est bien affiché");
+			SocleTechnique.assertEqualsLogger("Le bouton Enregistrer le projet n'est pas représenté par le bonne icone", "http://localhost:8090/libreplan/common/img/ico_save.png", detailProjet.buttonEnregistrerProjet.getAttribute("src"), logger);
+			logger.info("Le bouton Enregistrer le projet est bien représenté par le bon icone");
+
+			
+			//_______________Step 7_______________ 
+			//Cliquer sur le bouton annuler l'édition, verifier l'affichage d'une alerte et de son message
+			detailProjet.buttonAnnulerEdition.click();
+			SocleTechnique.assertTrueLogger("La fenêtre d'alerte ne s'affiche pas", detailProjet.alertSortie.isDisplayed(), logger);
+			logger.info("La fenêtre d'alerte est bien affichée");
+			SocleTechnique.assertEqualsLogger("Le message de l'alerte n'est pas conforme", "Les modifications non enregistrées seront perdues. Êtes-vous sûr ?", detailProjet.alertSortie.getText(), logger);
+			logger.info("Le message de l'alerte est bien conforme");
+			SocleTechnique.assertTrueLogger("Le bouton OK de l'alerte n'est pas present", detailProjet.buttonAlertOK.isDisplayed(), logger);
+			logger.info("Le bouton OK de l'alerte est bien affiché");
+			SocleTechnique.assertTrueLogger("Le bouton Annuler de l'alerte n'est pas present", detailProjet.buttonAlertAnnuler.isDisplayed(), logger);
+			logger.info("Le bouton Annuler de l'alerte est bien affiché");
 			
 			
-			
-			
-			
-			
-			
+			//_______________Step 7_______________ 
+			//Cliquer sur le bouton annuler de l'alerte annuler l'édition
+			detailProjet.buttonAlertAnnuler.click();
+			//Demander à Arielle !!!!!//SocleTechnique.assertFalseLogger("La fenêtre d'alerte est toujours affichée", detailProjet.alertSortie.isDisplayed(), logger);
+			//logger.info("La fenêtre d'alerte est bien fermée");
 		}
 	
 }
